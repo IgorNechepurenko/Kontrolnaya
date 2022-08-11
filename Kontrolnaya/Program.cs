67d34d528e67cc74ef1f015866196ecc995ece8b
userName <nahalenok33@bk.ru>
2022-08-11 19:10:34 +0800

    задача

--- Kontrolnaya/Program.cs
+++ Kontrolnaya/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿string[] a = new string[4] {"1234", "1567", "-2", "computer science"};
+string[] b = new string[a.Length];
+void f(string[] a, string[] b)
+{
+    int count = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+    if(a[i].Length <= 3)
+        {
+        b[count] = a[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+f(a, b);
+PrintArray(b);
+
